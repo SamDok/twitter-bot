bdad0e8b29f05dd10ab25bd731e528949bacd1b7
SamDok <samridh.dokania@gmail.com>
2018-09-13 00:53:44 +0530

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -29,9 +29,10 @@ const SendMessage = user => {
     }, timeout);
   }
 };
-return `Hey,
-        I noticed that you love Illenium. :D`; // your message
+const GenerateMessage = name => {
+  return `Hey,
+          I noticed that you love ILLENIUM! :D`; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
-
+};
 
 module.exports = AutoDM;
